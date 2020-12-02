36a1f50142855631c5ee8792fcd795ffc17e3f61
TOM-ATIV9\Tom <thomyerk@gmail.com>
2020-12-02 15:43:16 +0100

    time rewrote to hours

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 var today = new Date();
 var day = String(today.getDate()).padStart(2, '0');
 var month = String(today.getMonth() + 1).padStart(2, '0'); 
-var time = today.getHours()
+var hours = today.getHours()
 var minutes = String(today.getMinutes() + 1).padStart(2, '0'); 
 var datum = today.getFullYear()+'-'+month+'-'+day
 
 var dateInput = document.querySelector("input[type='date']")
 dateInput.setAttribute("min",datum)
 var timeInput = document.querySelector("input[type='time']")
-timeInput.setAttribute("min",(time+1) + ':' + minutes)
+timeInput.setAttribute("min",(hours+1) + ':' + minutes)
 
 var submitButton = document.querySelector('#submit')
 var emailError = document.querySelector('#email-error')
