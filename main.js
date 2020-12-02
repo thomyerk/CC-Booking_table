d23fd7155548c49bb22ccc1319a2eea74c79b0e9
TOM-ATIV9\Tom <thomyerk@gmail.com>
2020-12-02 20:36:24 +0100

    booking modal added

--- main.js
+++ main.js
@@ -3,26 +3,54 @@ var day = String(today.getDate()).padStart(2, '0');
 var month = String(today.getMonth() + 1).padStart(2, '0'); 
 var hours = today.getHours()
 var minutes = String(today.getMinutes() + 1).padStart(2, '0'); 
-var datum = today.getFullYear()+'-'+month+'-'+day
+var todaysDate = today.getFullYear()+'-'+month+'-'+day
 
 var dateInput = document.querySelector("input[type='date']")
-dateInput.setAttribute("min",datum)
+dateInput.setAttribute("min",todaysDate)
 var timeInput = document.querySelector("input[type='time']")
-timeInput.setAttribute("min",(hours+1) + ':' + minutes)
 
-var submitButton = document.querySelector('#submit')
+if(dateInput.value == todaysDate){
+    timeInput.setAttribute("min",(hours+1) + ':' + minutes)
+}
+
+var submitButton = document.querySelector('#book')
 var emailError = document.querySelector('#email-error')
 submitButton.addEventListener("click",ValidateEmail)
+var modal = document.getElementById("myModal");
 
-function ValidateEmail(emailInput)
+function ValidateEmail(event, emailInput)
 {
+    event.preventDefault()
+    var bookingData = document.querySelector('#bookingData')
     emailInput = document.querySelector('#email').value
     var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     if(emailInput.match(mailformat)){
-        //kiiratni az adatokat
+        for(let elements of form){
+            console.log(elements.value)
+            modal.style.display = "block";
+            let row = document.createElement("p")
+            row.innerHTML = elements.value
+            document.getElementById("formResults").appendChild(row)
+        }
     }else{
-        emailError.innerHTML="Wrong email format";
+        emailError.innerHTML="Wrong email format ";
         return false;
     }
 }
+
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 
