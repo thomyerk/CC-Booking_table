356e6a2df4315d2c4d89779a6fa401e4b13538ba
TOM-ATIV9\Tom <thomyerk@gmail.com>
2020-12-02 20:37:57 +0100

    booking modal problem fixed

--- main.js
+++ main.js
@@ -43,6 +43,7 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
+    location.reload()
 }
 
 
